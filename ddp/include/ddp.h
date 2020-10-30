40401c2d11e04f5540009b0409cc088c80dce6ec
Lasitha Wijayarathne <lwijayarathne3@gatech.edu>
2020-10-30 14:13:39 -0400

    added subdirectories for better organization

---
+++ ddp/include/ddp.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <list>
+
+#include <Eigen/Dense>
+
+
+#include "config.h"
+// #include "spline.h"
+#include "ilqrsolver.h"
+#include "kuka_arm.h"
+#include "SoftContactModel.h"
+#include "KukaModel.h"
+#include "models.h"
+
+
+using namespace std;
+using namespace Eigen;
+
+/* DDP trajectory generation */
+
+// static std::list< const char*> gs_fileName;
+// static std::list< std::string > gs_fileName_string;
+
+
+/* -------------------- Soft_contact_state = 17(14+3) ------------------------*/
+class DDP 
+{
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  DDP();
+
+  void run(stateVec_t xinit, stateVec_t xgoal, stateVecTab_t xtrack);
+
+
+
+private:
+  stateVecTab_t joint_state_traj;
+  commandVecTab_t torque_traj;
+  stateVecTab_t joint_state_traj_interp;
+  commandVecTab_t torque_traj_interp;
+
+protected:
+  optimizer::ILQRSolver::traj lastTraj;
+
+};
+
+
+
+
+
