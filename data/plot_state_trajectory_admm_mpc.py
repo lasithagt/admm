7be8407a39a8bd82345b322795fc2662cdaba2ea
lasitha <lasithagt@gmail.com>
2021-01-12 16:22:40 -0500

    ploting functions

--- data/plot_state_trajectory_admm_mpc.py
+++ data/plot_state_trajectory_admm_mpc.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 	# b = np.load('./state_trajectory_admm_mpc.npy')
 	data_des = np.load('./state_trajectory_admm_mpc_desired.npy')
 	data_mpc = np.load('./state_trajectory_admm_mpc.npy')
+	data_mpc_state = np.load('./state_trajectory_admm_state_mpc.npy')
 
 	
 	print(data_mpc.shape)
@@ -17,6 +18,9 @@ if __name__ == "__main__":
 
 	data_mpc = data_mpc.squeeze()
 	data_mpc = data_mpc.T
+
+	data_mpc_state = data_mpc_state.squeeze()
+	data_mpc_state = data_mpc_state.T
 
 
 	# line_c = ['b', 'g', 'r', 'c', 'k', 'm', 'y']
@@ -33,7 +37,7 @@ if __name__ == "__main__":
 	ax.plot3D(xline, yline, zline, label='Desired')
 	ax.scatter3D(xline[0], yline[0], zline[0], 'r+')
 
-	print(data_mpc)
+	# print(data_mpc)
 	# fig1 = plt.figure(2)
 	# ax = plt.axes(projection='3d')
 
@@ -43,6 +47,14 @@ if __name__ == "__main__":
 
 	ax.plot3D(xline, yline, zline, label='Actual')
 	ax.scatter3D(xline[0], yline[0], zline[0], 'b+')
+
+
+	xline = data_mpc_state[0,::]
+	yline = data_mpc_state[1,::]
+	zline = data_mpc_state[2,::]
+
+	ax.plot3D(xline, yline, zline, label='Actual_state')
+	ax.scatter3D(xline[0], yline[0], zline[0], 'k+')
 
 	plt.legend()
 
---
+++ data/plot_state_trajectory_admm_test.py
@@ -0,0 +1,45 @@
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+if __name__ == "__main__":
+	data = np.array(np.load('./state_trajectory_admm_mpc_test.npy'))
+
+	data = data.squeeze()
+	data = data.T
+
+	fig1 = plt.figure(1)
+	ax = plt.axes(projection='3d')
+	# ax.set_aspect('equal')
+
+
+	# Plot data for motion
+	# for i in range(6):
+	xline = data[0,::]
+	yline = data[1,::]
+	zline = data[2,::]
+	# line_name = 'Iteration' + str(5)
+	ax.plot3D(xline, yline, zline)
+
+	# ax.legend()
+
+	# fig2 = plt.figure(2)
+
+
+	# Plot data for force
+	# x_time = np.arange(0, data.shape[2], 1)
+
+	# xline = data[4,3,::]
+	# yline = data[4,4,::]
+	# zline = data[4,5,::]
+	# plt.plot(x_time, xline, label='Force X')
+	# plt.plot(x_time, yline, label='Force Y')
+	# plt.plot(x_time, zline, label='Force Z')
+
+	# plt.legend()
+
+
+	plt.show()
+	# print(data.shape) 
